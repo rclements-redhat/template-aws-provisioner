73b05f9854bced20e440662b00b55902a24b482e
Ryan Clements <rclement@redhat.com>
2023-03-30 00:02:56 -0400

    Write a lot of new files create the AWS infrastructure provisioning engine.

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
 pip install --upgrade pip
-python -m pip install -r ../requirements.txt
-ansible-galaxy collection install -r ../requirements.yml
+python -m pip install -r "${SCRIPT_DIR}/../requirements.txt"
+ansible-galaxy collection install -r "${SCRIPT_DIR}/../requirements.yml"
